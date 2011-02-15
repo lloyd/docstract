30477543e1319756731e58fbdd69d1e44169762e
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-02-15 14:24:39 -0700

    a test of recurring tags

---
+++ tests/description_recurring.js
@@ -0,0 +1,19 @@
+/**
+ * Description may recur.  lots of times.
+ *
+ * @desc Here's a little bit.
+ * @description
+ * Here's a little bit more.
+ *
+ * @desc and how bout a final bit of module desc?
+ */
+
+/**
+ * @function foo
+ * The function that's documented all over because in functions...
+ * @param {string} bar An AWESOME param. 
+ * @description
+ * ..description can be interspersed...
+ * @param {string} baz Another AWESOME param.
+ * @desc between other tags!
+ */
