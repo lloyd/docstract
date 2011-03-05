e14a52ada00948ecc325cef62e8a7737b311bafa
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-03-04 18:41:37 -0700

    test of type parsing, we're a bit of a floozy at the moment...

---
+++ tests/fun_with_types.js
@@ -0,0 +1,17 @@
+/**
+ * This is an array of strings
+ * @type {array of strings}
+ */
+exports.arrayOfStrings = [ "foo", "bar", "baz" ];
+
+/**
+ * This is an another array of strings
+ * @type {strings[]}
+ */
+exports.arrayOfStrings2 = [ "foo", "bar", "baz" ];
+
+/**
+ * This is a *third* array of strings
+ * @type { Strings [ ] }
+ */
+exports.arrayOfStrings3 = [ "foo", "bar", "baz" ];
